004bf6d59f7942505ba1613563ed95f2360f94f3
Douglas Matos Gomes <dsmsgomes@gmail.com>
2021-09-02 02:47:53 -0300

    Optimism bonus for material. Bench 5288999

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1092,11 +1092,12 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
+         int mat = pos.non_pawn_material();
          int scale =   883
                      + 32 * pos.count<PAWN>()
-                     + 32 * pos.non_pawn_material() / 1024;
+                     + 32 * mat / 1024;
 
-         Value nnue = NNUE::evaluate(pos, true) * scale / 1024;
+         Value nnue = NNUE::evaluate(pos, true) * scale / 1024 + mat / 512;
 
          if (pos.is_chess960())
              nnue += fix_FRC(pos);
