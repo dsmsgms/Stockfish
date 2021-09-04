00f4770440a8a2b693aadb7fea6e3ef797de5273
Douglas Matos Gomes <dsmsgomes@gmail.com>
2021-09-04 06:22:26 -0300

    Quadratic decay when shuffling. Bench: 5720606

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1115,7 +1115,8 @@ Value Eval::evaluate(const Position& pos) {
   }
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (100 - pos.rule50_count()) / 100;
+  int to_r50 = 100 - pos.rule50_count();
+  v = v * to_r50 * to_r50 / 10000;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
