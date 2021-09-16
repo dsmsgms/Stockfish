0b43aa8427edb23e5806e3854e3a6d723ebe403b
Douglas Matos Gomes <dsmsgomes@gmail.com>
2021-09-16 01:03:42 -0300

    Use psqt as classical eval. Bench: 6659650

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1107,11 +1107,10 @@ Value Eval::evaluate(const Position& pos) {
       // If there is PSQ imbalance we use the classical eval, but we switch to
       // NNUE eval faster when shuffling or if the material on the board is high.
       int r50 = pos.rule50_count();
-      Value psq = Value(abs(eg_value(pos.psq_score())));
-      bool classical = psq * 5 > (850 + pos.non_pawn_material() / 64) * (5 + r50);
+      Value psq = Value(eg_value(pos.psq_score()));
+      bool classical = abs(psq) * 5 > (850 + pos.non_pawn_material() / 64) * (5 + r50);
 
-      v = classical ? Evaluation<NO_TRACE>(pos).value()  // classical
-                    : adjusted_NNUE();                   // NNUE
+      v = classical ? (pos.side_to_move() == WHITE ? psq : -psq) : adjusted_NNUE();
   }
 
   // Damp down the evaluation linearly when shuffling
