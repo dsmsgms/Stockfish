788eefa968a75ce220179006a545c465fff0648c
Douglas Matos Gomes <dsmsgomes@gmail.com>
2021-11-18 03:06:10 -0300

    Use more time in critical positions. Take 2. Bench: 6339548

--- src/search.cpp
+++ src/search.cpp
@@ -493,8 +493,9 @@ void Thread::search() {
           }
           double bestMoveInstability = 1.073 + std::max(1.0, 2.25 - 9.9 / rootDepth)
                                               * totBestMoveChanges / Threads.size();
-					if (abs(bestValue) >= 9*PawnValueMg/10 && abs(bestValue) <= 15*PawnValueMg/10)
-						bestMoveInstability *= bestMoveInstability;
+          if (abs(bestValue) >= PawnValueMg && abs(bestValue) <= 13*PawnValueMg/10) {
+            bestMoveInstability *= 1.09 + totBestMoveChanges / Threads.size();
+          }
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability;
 
           // Cap used time in case of a single legal move for a better viewer experience in tournaments
