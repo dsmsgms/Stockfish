48002f849497d63ee3f87e72cdc579836c4d9316
Douglas Matos Gomes <dsmsgomes@gmail.com>
2022-01-11 01:24:32 -0300

    Only use classical eval with big material advantage. Bench: 4690313

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1087,7 +1087,8 @@ Value Eval::evaluate(const Position& pos) {
   // Deciding between classical and NNUE eval (~10 Elo): for high PSQ imbalance we use classical,
   // but we switch to NNUE during long shuffling or with high material on the board.
   if (  !useNNUE
-      || abs(eg_value(pos.psq_score())) * 5 > (849 + pos.non_pawn_material() / 64) * (5 + pos.rule50_count()))
+      || (abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK)) >= KnightValueMg
+			&& abs(eg_value(pos.psq_score())) * 5 > (849 + pos.non_pawn_material() / 64) * (5 + pos.rule50_count())))
   {
       v = Evaluation<NO_TRACE>(pos).value();          // classical
       useClassical = abs(v) >= 298;
