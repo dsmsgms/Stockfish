224263fa8098cee9570642c8f07ce0ca0c3ff22b
Douglas Matos Gomes <dsmsgomes@gmail.com>
2022-11-20 22:26:23 -0300

    Tweak ocb endgames (Take 2). Bench: 4204779

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1082,6 +1082,14 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
 
       optimism = optimism * (269 + nnueComplexity) / 256;
       v = (nnue * scale + optimism * (scale - 754)) / 1024;
+
+      Color strongSide = nnue > 0 ? stm : ~stm;
+      if (    pos.opposite_bishops()
+           && (   pos.non_pawn_material() == 2*BishopValueMg+2*RookValueMg
+               || pos.non_pawn_material() == 2*BishopValueMg)
+           && std::abs(v) < 150*PawnValueEg/100
+           && pos.count<PAWN>(strongSide) == 1+pos.count<PAWN>(~strongSide))
+          v = (3 * v) / 4;
   }
 
   // Damp down the evaluation linearly when shuffling
