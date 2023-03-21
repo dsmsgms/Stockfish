575352e5a85cff621285c4d276ad8a520636b489
Douglas Matos Gomes <dsmsgomes@gmail.com>
2023-03-21 02:13:35 -0300

    Speedup LMR reduction change. No functional change. Bench: 4980082

--- src/search.cpp
+++ src/search.cpp
@@ -1147,43 +1147,32 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction if position is or has been on the PV
       // and node is not likely to fail low. (~3 Elo)
-      if (   ss->ttPv
-          && !likelyFailLow)
-          r -= 2;
+      r -= (ss->ttPv && !likelyFailLow)*2;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
-      if ((ss-1)->moveCount > 7)
-          r--;
+      r -= (ss-1)->moveCount > 7;
 
       // Increase reduction for cut nodes (~3 Elo)
-      if (cutNode)
-          r += 2;
+      r += cutNode*2;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
-      if (ttCapture)
-          r++;
+      r += ttCapture;
 
       // Decrease reduction for PvNodes based on depth
       if (PvNode)
           r -= 1 + 12 / (3 + depth);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
-      if (singularQuietLMR)
-          r--;
+      r -= singularQuietLMR;
 
       // Decrease reduction if we move a threatened piece (~1 Elo)
-      if (   depth > 9
-          && (mp.threatenedPieces & from_sq(move)))
-          r--;
+      r -= (depth > 9 && (mp.threatenedPieces & from_sq(move)));
 
       // Increase reduction if next ply has a lot of fail high
-      if ((ss+1)->cutoffCnt > 3)
-          r++;
+      r += ((ss+1)->cutoffCnt > 3);
 
       // Decrease reduction if move is a killer and we have a good history
-      if (move == ss->killers[0]
-          && (*contHist[0])[movedPiece][to_sq(move)] >= 3722)
-          r--;
+      r -= (move == ss->killers[0] && (*contHist[0])[movedPiece][to_sq(move)] >= 3722);
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
@@ -1192,7 +1181,10 @@ moves_loop: // When in check, search starts here
                      - 4182;
 
       // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-      r -= ss->statScore / (11791 + 3992 * (depth > 6 && depth < 19));
+      if (depth > 6 && depth < 19)
+          r -= ss->statScore / 15783;
+      else
+          r -= ss->statScore / 11791;
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
@@ -1238,8 +1230,7 @@ moves_loop: // When in check, search starts here
       else if (!PvNode || moveCount > 1)
       {
           // Increase reduction for cut nodes and not ttMove (~1 Elo)
-          if (!ttMove && cutNode)
-              r += 2;
+          r += (!ttMove && cutNode)*2;
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
       }
