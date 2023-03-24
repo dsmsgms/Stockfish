b541f8003f6ad11496d8509d5a2dcca68fab080b
Douglas Matos Gomes <dsmsgomes@gmail.com>
2023-03-24 06:45:33 -0300

    Speedup LMR reduction change (Take 2). No functional change. Bench: 4980082

--- src/search.cpp
+++ src/search.cpp
@@ -1192,7 +1192,10 @@ moves_loop: // When in check, search starts here
                      - 4182;
 
       // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-      r -= ss->statScore / (11791 + 3992 * (depth > 6 && depth < 19));
+      if (depth > 6 && depth < 19)
+        r -= ss->statScore / 15783;
+      else
+        r -= ss->statScore / 11791;
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
