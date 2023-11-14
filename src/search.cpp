ad284175ab1cb22bdbe883c6543a1cd49fb71ebe
Douglas Matos Gomes <dsmsgomes@gmail.com>
2023-11-14 16:17:11 -0300

    Do check extension if side to move has a queen. Bench: 1167049

--- src/search.cpp
+++ src/search.cpp
@@ -1093,7 +1093,7 @@ moves_loop:  // When in check, search starts here
             }
 
             // Check extensions (~1 Elo)
-            else if (givesCheck && depth > 9)
+            else if (givesCheck && pos.count<QUEEN>(pos.side_to_move()) && depth > 9)
                 extension = 1;
 
             // Quiet ttMove extensions (~1 Elo)
